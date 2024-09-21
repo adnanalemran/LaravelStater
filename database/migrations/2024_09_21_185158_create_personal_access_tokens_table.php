cae7d3c826dae37e2c80b7efab39e3a21df193bb
adnanalemran <adnanalemranontor@gmail.com>
2024-09-22 00:56:22 +0600

    api env

---
+++ database/migrations/2024_09_21_185158_create_personal_access_tokens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       if (!Schema::hasTable('personal_access_tokens')) {
+            Schema::create('personal_access_tokens', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('tokenable_id')->constrained();
+                $table->string('tokenable_type')->index();
+                $table->string('name');
+                $table->text('token');
+                $table->text('abilities')->nullable();
+                $table->timestamp('last_used_at')->nullable();
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personal_access_tokens');
+    }
+};
